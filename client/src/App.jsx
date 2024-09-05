8bc82180b17cb0436dba2945c91bd4b16aa2d98b
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-09-05 22:29:20 +0200

    Client: Clean App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { Route, Routes } from "react-router-dom";
 
 import MainLayout from "./components/layout/MainLayout/MainLayout.jsx";
@@ -7,8 +5,6 @@ import ExplorePage from "./components/pages/Explore/ExplorePage.jsx";
 import LoginPage from "./components/pages/LoginPage/LoginPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <MainLayout>
       <Routes>
